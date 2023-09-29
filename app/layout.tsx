857307a6be777f9d1568c62921b8529d68b3ae5f
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-09-29 17:49:57 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Learning Game",
+  description: "A learning game for kids",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
